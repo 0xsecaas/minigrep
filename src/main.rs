098db77befd2a79a6b734c41e55f406e80c77a71
Charly <github.womankind045@passinbox.com>
2025-06-13 23:39:11 +0330

    refactor improve excption handling

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,16 @@ use std::{env, error::Error, process};
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        println!("Input error: {err}");
         process::exit(1);
     });
 
-    read_file(&config.file_path);
-    Ok(())
-}
+    if let Err(e) = run(&config) {
+        println!("Application error: {e}");
+        process::exit(2);
+    }
 
-fn read_file(path: &str) {
-    use std::fs;
-    let contents = fs::read_to_string(path).expect("failed to read the file");
-    println!("content are:\n{contents}");
+    Ok(())
 }
 
 struct Config<'a> {
@@ -24,7 +22,7 @@ struct Config<'a> {
 
 impl<'a> Config<'a> {
     fn build(args: &'a [String]) -> Result<Self, &'static str> {
-        if args.len() < 2 {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
         Ok(Self {
@@ -32,4 +30,16 @@ impl<'a> Config<'a> {
             file_path: &args[2],
         })
     }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let file_contents = read_file(config.file_path)?;
+    println!("content are:\n{file_contents}");
+    Ok(())
+}
+
+fn read_file(path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    use std::fs;
+    let contents = fs::read_to_string(path)?;
+    Ok(contents)
 }
