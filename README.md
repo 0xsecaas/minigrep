b4b089f4c101987694fea2e2a1915ee0b07bc57f
Charly <github.womankind045@passinbox.com>
2025-06-13 22:58:55 +0330

    first refactor

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ find specific string in a specific file using command line.
 Features:
 
 - Accepting Command Line Arguments
+
+  - arguments:
+    1. the file path
+    2. a string to search for
+
 -
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env::{self, args};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
+    read_file(&config.file_path);
+}
+
+fn read_file(path: &str) {
+    use std::fs;
+    let contents = fs::read_to_string(path).expect("failed to read the file");
+    println!("content are:\n{contents}");
+}
+
+struct Config<'a> {
+    query: &'a String,
+    file_path: &'a String,
+}
+
+impl<'a> Config<'a> {
+    fn new(args: &'a [String]) -> Self {
+        Self {
+            query: &args[1],
+            file_path: &args[2],
+        }
+    }
 }
