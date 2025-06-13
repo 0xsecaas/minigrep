557137111fac4f58d4f2d8d48e89370cc832c92d
Charly <github.womankind045@passinbox.com>
2025-06-13 23:23:41 +0330

    refactor excption handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::env::{self, args};
+use std::{env, error::Error, process};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     read_file(&config.file_path);
+    Ok(())
 }
 
 fn read_file(path: &str) {
@@ -19,10 +23,13 @@ struct Config<'a> {
 }
 
 impl<'a> Config<'a> {
-    fn new(args: &'a [String]) -> Self {
-        Self {
+    fn build(args: &'a [String]) -> Result<Self, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        Ok(Self {
             query: &args[1],
             file_path: &args[2],
-        }
+        })
     }
 }
