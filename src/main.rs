7f3079e3ae9ef03126ed8c673c88a28ad9bbd37f
Charly <github.womankind045@passinbox.com>
2025-06-14 13:38:20 +0330

    fix(output): print errors on standard error

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{env, error::Error, process};
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let config: Config<'_> = Config::build(&args).unwrap_or_else(|err: &'static str| {
-        println!("Input error: {err}");
+        eprintln!("Input error: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(&config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(2);
     }
 
