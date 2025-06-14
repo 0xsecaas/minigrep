8859eefc538d436a28316fc838ed2f355a203d61
Charly <github.womankind045@passinbox.com>
2025-06-14 12:00:03 +0330

    feat(search): with tdd

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,11 @@ impl<'a> Config<'a> {
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let file_contents = read_file(config.file_path)?;
-    println!("content are:\n{file_contents}");
+
+    for line in search(&config.query, &file_contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
@@ -27,4 +31,32 @@ fn read_file(path: &str) -> Result<String, Box<dyn std::error::Error>> {
     use std::fs;
     let contents = fs::read_to_string(path)?;
     Ok(contents)
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
